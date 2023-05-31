db62600d2a5a5e4a848de40c7011e49d5a35c0ed
23navi <navisureka23@gmail.com>
2023-06-01 00:22:26 +0530

    skaffold added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,15 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/:value", (req, res) => {
+  res.send("Hi there");
+});
+
+app.get("*", (req, res) => {
+  console.log("this is reached");
+  res.send("yooo");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000 for the auth service");
 });
