45805351e44af3d12774ef6a9726521a718e5c8d
23navi <navisureka23@gmail.com>
2023-07-29 21:50:44 +0530

    added tdd

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,7 +8,7 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect("mongodb://tickets-mongo-srv:27017/auth");
+    await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets");
 
     console.log("Ticket Service: Connected to MongoDb");
   } catch (err) {
