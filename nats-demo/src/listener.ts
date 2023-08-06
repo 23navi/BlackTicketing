33af732a3aaa000e787ac344304d601aa3f76c83
23navi <navisureka23@gmail.com>
2023-08-06 13:46:24 +0530

    listener

--- nats-demo/src/listener.ts
+++ nats-demo/src/listener.ts
@@ -1,10 +1,19 @@
 import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 // It is called stan bec of community convention... the better name would be "client", which allows to connect to nats streaming server from nodejs
-const stan = nats.connect("ticketing", "abcde", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
+
+  const subscription = stan.subscribe("ticket:created", "my-queue-group-1");
+
+  subscription.on("message", (msg: any) => {
+    console.log(
+      `Message received with sequence number ${msg.getSequence()}. Data: ${msg.getData()} `
+    );
+  });
 });
