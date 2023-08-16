2b12b3ee59bfa6c04337f13ee2226eda61de693e
23navi <navisureka23@gmail.com>
2023-08-16 09:11:19 +0530

    update the imports from btcommon

--- nats-demo/src/events/Event.ts
+++
@@ -1,5 +0,0 @@
-import { Subjects } from "./subjects";
-export default interface IEvent {
-  subject: Subjects;
-  data: any;
-}
--- nats-demo/src/events/ticket-created-publisher.ts
+++ nats-demo/src/events/ticket-created-publisher.ts
@@ -1,6 +1,6 @@
-import { BasePublisher } from "./base-publisher";
-import { Subjects } from "./subjects";
-import ITicketCreatedEvent from "./ticket-created-event";
+import { BasePublisher } from "@23navi/btcommon";
+import { Subjects } from "@23navi/btcommon";
+import { ITicketCreatedEvent } from "@23navi/btcommon";
 
 export class TicketCreatedPublisher extends BasePublisher<ITicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
