acd9aac0e4ca1c99da8b0093c0f69fe0a92e98e9
23navi <navisureka23@gmail.com>
2023-08-16 08:39:31 +0530

    add async-await support to publisher

--- nats-demo/src/events/base-publisher.ts
+++ nats-demo/src/events/base-publisher.ts
@@ -9,10 +9,14 @@ export abstract class BasePublisher<T extends IEvent> {
 
   //   public abstract data: T["data"];
 
-  publish(data: T["data"]) {
-    this.client.publish(this.subject, JSON.stringify(data)),
-      () => {
-        console.log("Event published");
-      };
+  publish(data: T["data"]): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data)),
+        (err: Error) => {
+          if (err) reject(err);
+          console.log("Event published");
+          resolve();
+        };
+    });
   }
 }
