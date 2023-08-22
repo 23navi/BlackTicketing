83896f20bd655ee554be22f71e448975c1070d8b
23navi <navisureka23@gmail.com>
2023-08-22 13:02:38 +0530

    add orderService enum

---
+++ orders/src/model/order.ts
@@ -0,0 +1,61 @@
+import mongoose, { Mongoose } from "mongoose";
+
+// An interface that describes the properties
+// that are requried to create a new Ticket
+interface OrderAttrs {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+}
+
+// An interface that describes the properties
+// that a Ticket Document has ... we can have mongo created properties too.. eg createdAt
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An interface that describes the properties
+// that a Ticket Model has
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+      required: false, // This is not required as for some orders we don't want to expire them.. eg: Order with is confirmed.. Expiration is just for orders which are in lockdown state.
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      require: true,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Ticket", orderSchema);
+
+export { Order };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 import { requireAuth, validateRequest } from "@23navi/btcommon";
 import { body } from "express-validator";
 import { natsWrapper } from "../nats-wrapper";
@@ -8,7 +9,16 @@ const router = express.Router();
 router.post(
   "/api/orders",
   requireAuth,
-  [body("ticketId")],
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      // Problem with this validation check for id being mongoose.. We got some dependency as we are saying that ticket id will be always a mongodb id.. ie: Ticket service will always have mongodb as the database..
+      .custom((input) => {
+        mongoose.Types.ObjectId.isValid(input);
+      })
+      .withMessage("TicketId must be provided"),
+  ],
   validateRequest, // To catch and throw errors caused by expressValidator
   async (req: Request, res: Response) => {
     const { ticketId } = req.body;
