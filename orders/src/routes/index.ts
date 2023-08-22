7745918355229419a77e64e25e73a94dbed40dc1
23navi <navisureka23@gmail.com>
2023-08-22 17:57:59 +0530

    index

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express";
 import { requireAuth } from "@23navi/btcommon";
+import { Order } from "../model/order";
 
 const router = express.Router();
 
 router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
-  return {};
+  //Only return the orders for the current use
+  const orders = Order.find({
+    userId: req.currentUser!.id,
+  }).populate("ticket");
+  return res.send(orders);
 });
 
 export { router as indexOrderRouter };
