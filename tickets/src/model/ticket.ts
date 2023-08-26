e2e9a9bac4f1662cdfc8638645f504c8ba0f19af
23navi <navisureka23@gmail.com>
2023-08-26 18:39:24 +0530

    add updateIfCurrentPlugin to ticket model

--- tickets/src/model/ticket.ts
+++ tickets/src/model/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 // An interface that describes the properties
 // that are requried to create a new Ticket
@@ -46,6 +47,9 @@ const ticketSchema = new mongoose.Schema(
     },
   }
 );
+
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
