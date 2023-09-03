6002ce59f91321e1445e90643734ac9825177ba5
23navi <navisureka23@gmail.com>
2023-09-03 20:25:37 +0530

    ticket orderId update on order:created event

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -3,7 +3,7 @@ import { OrderCancelledEvent } from "@23navi/btcommon";
 import { Message } from "node-nats-streaming";
 import QUEUEGROUPNAME from "./queue-group-name";
 
-export class OrderCreatedListener extends BaseListener<OrderCancelledEvent> {
+export class OrderCancelledListener extends BaseListener<OrderCancelledEvent> {
   readonly subject = Subjects.OrderCancelled;
   queueGroupName = QUEUEGROUPNAME;
 
--- tickets/src/model/ticket.ts
+++ tickets/src/model/ticket.ts
@@ -16,6 +16,7 @@ interface TicketDoc extends mongoose.Document {
   price: number;
   userId: string;
   version: number;
+  orderId?: string;
 }
 
 // An interface that describes the properties
@@ -37,6 +38,10 @@ const ticketSchema = new mongoose.Schema(
     userId: {
       type: String,
       require: true,
+    },
+    orderId: {
+      type: String,
+      required: false,
     },
   },
   {
