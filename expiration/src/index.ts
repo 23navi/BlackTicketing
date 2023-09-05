b672d6095f24db3b707b3e0e5c0b739fbd0d87e6
23navi <navisureka23@gmail.com>
2023-09-05 13:50:07 +0530

    expiration service skaffold and k8s

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,17 +1,6 @@
-import mongoose from "mongoose";
-
 import { natsWrapper } from "./nats-wrapper";
 
-import { app } from "./app";
-import { OrderCreatedListener } from "./events/listeners/order-created-listener";
-
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
-  }
-  if (!process.env.MONGO_URL) {
-    throw new Error("MONGO_URL must be defined");
-  }
   if (!process.env.NATS_URL) {
     throw new Error("NATS_URL must be defined");
   }
@@ -23,22 +12,14 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URL!);
-
-    console.log("Ticket Service: Connected to MongoDb");
-  } catch (err) {
-    console.log("Cannot connect to MongoDb from Tickets service");
-    console.error(err);
-  }
-  try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID!,
       process.env.NATS_CLIENT_ID!,
       process.env.NATS_URL!
     );
-    console.log("Ticket Service: Connected to Nats");
+    console.log("Expiration Service: Connected to Nats");
     natsWrapper.client.on("close", () => {
-      console.log("Nats connection closed in Ticket Service");
+      console.log("Nats connection closed in Expiration Service");
       process.exit();
     });
 
@@ -46,16 +27,11 @@ const start = async () => {
     process.on("SIGTERM", () => natsWrapper.client.close());
 
     //Listeners
-    new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) {
-    console.log("Cannot connect to Nats in Ticket Service");
+    console.log("Cannot connect to Nats in Expiration Service");
     process.exit(1);
     // console.error(err);
-  }
-
-  app.listen(3000, () => {
-    console.log("Ticket Service: Listening on port 3000!");
-  });
+  } 
 };
 
 start();
