e5854e2eb4fe23f7be5afd629c06cd3aaaa9a44e
23navi <navisureka23@gmail.com>
2023-09-14 10:19:02 +0530

    bull job process and emit

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,15 +2,16 @@ import { BaseListener, NotFoundError, Subjects } from "@23navi/btcommon";
 import { OrderCreatedEvent } from "@23navi/btcommon";
 import { Message } from "node-nats-streaming";
 import QUEUEGROUPNAME from "./queue-group-name";
-
+import { expirationQueue } from "../../queue/expiration-queue";
 export class OrderCreatedListener extends BaseListener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = QUEUEGROUPNAME;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    console.log("Event data on Create! ", data);
+    console.log("Event data on Create from expiration service! ", data);
 
     // Ack the message after saving the ticket
+    expirationQueue.add({ orderId: data.id });
     msg.ack();
   }
 }
