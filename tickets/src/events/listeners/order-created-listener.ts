a164d524e34af92651cfe3bb334cb9e119d585f6
23navi <navisureka23@gmail.com>
2023-09-17 09:52:01 +0530

    TicketUpdatePublisher setup

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,6 +3,7 @@ import { OrderCreatedEvent } from "@23navi/btcommon";
 import { Message } from "node-nats-streaming";
 import QUEUEGROUPNAME from "./queue-group-name";
 import { Ticket } from "../../model/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends BaseListener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -10,7 +11,7 @@ export class OrderCreatedListener extends BaseListener<OrderCreatedEvent> {
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
     console.log("Event data on Create! ", data);
- 
+
     // 1 find the ticket with ticket id in data
     const ticket = await Ticket.findById(data.ticket.id);
 
@@ -28,6 +29,18 @@ export class OrderCreatedListener extends BaseListener<OrderCreatedEvent> {
     console.log(
       "Ticket orderId update on order create event in ticket service"
     );
+
+    // We have updated the ticket in ticket service, now to maintain consistency, we will have to publish an event to tell other services to update their local copy of ticket.
+
+    // 5 publish the event (We have this.client as we made our BaseListener class this.client to be protected)
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
 
     // Ack the message after saving the ticket
     msg.ack();
