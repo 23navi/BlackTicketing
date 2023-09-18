36a1020e22996f4dddb2cf019c91fe77d49a68c5
23navi <navisureka23@gmail.com>
2023-09-18 09:43:48 +0530

    add order-cancelled-listener and test

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,38 @@
+import {
+  BaseListener,
+  Subjects,
+  NotFoundError,
+  orderStatus,
+} from "@23navi/btcommon";
+import { OrderCancelledEvent } from "@23navi/btcommon";
+import { Message } from "node-nats-streaming";
+import QUEUEGROUPNAME from "./queue-group-name";
+import { Order } from "../../models/orders";
+
+// On order:cancelled event, we will update the order status to cancelled.
+
+export class OrderCancelledListener extends BaseListener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+  queueGroupName = QUEUEGROUPNAME;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    console.log("Event data on Order Cancelled! ", data);
+
+    // 1 find the order with order id in data
+    const order = await Order.findById(data.id);
+
+    // 2 if no order, throw error..
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    //3 Update the order status to cancelled
+    order.set({ status: orderStatus.Cancelled });
+
+    //4 Save the order
+    await order.save();
+
+    // Ack the message after saving the ticket and publishing the event
+    msg.ack();
+  }
+}
